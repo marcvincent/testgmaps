7e6f300a32e9a27fef63c9615c14f806a95a6eb0
marcantoine.vincent@geoaddress.me <marcantoine.vincent@geoaddress.me>
2012-10-30 15:25:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testgmaps::Application.config.secret_token = '0b45983afcedd64283389cad708aa8b615d71ab76e50df41bd7a7b549607daca05d4e26290852867301db65b382a547214a1aa2986e9a892c856f28bd48f648d'
